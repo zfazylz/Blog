16cc4a4d43f00449f8773ab3c8a8a10d63c40957
zfazylz <zhfazylzhan@trafficwave.kz>
2020-12-20 08:17:57 +0600

    Migrations added

--- Blog/Startup.cs
+++ Blog/Startup.cs
@@ -25,38 +25,50 @@ namespace Blog
         {
             _config = config;
         }
-
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(_config["DefaultConnection"]));
+            services.AddDbContext<AppDbContext>(options => 
+                    options.UseSqlite(_config.GetConnectionString("DefaultConnection"))
+                );
             services.AddIdentity<IdentityUser, IdentityRole>(options =>
-                {
-                    options.Password.RequireDigit = false;
-                    options.Password.RequireNonAlphanumeric = false;
-                    options.Password.RequireUppercase = false;
-                    options.Password.RequiredLength = 6;
-                })
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 6;
+            })
                 .AddEntityFrameworkStores<AppDbContext>();
-            services.ConfigureApplicationCookie(options => { options.LoginPath = "/Auth/Login"; });
+                //.AddDefaultTokenProviders();
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/Auth/Login";
+            });
             services.AddTransient<IRepository, Repository>();
             services.AddTransient<IFileManager, FileManager>();
             services.AddMvc();
         }
 
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            
             app.UseRouting();
-
+            
             app.UseAuthentication();
             app.UseAuthorization();
-            app.UseStaticFiles();
+            app.UseStaticFiles();       
             app.UseEndpoints(endpoints =>
             {
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
                 endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}");
             });
         }
